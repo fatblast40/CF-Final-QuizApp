a12b8714e32b069e0c1827adc514f0dad933ec12
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-05-12 18:24:39 +0200

    refactor file submission

--- includes/upload_file.php
+++ includes/upload_file.php
@@ -10,12 +10,13 @@
 
 
 <?php
+if(isset($_POST["submit"])) {
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
